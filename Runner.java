9eb717bc9ce1b2c457103f97dd7fa5327cf99f04
Usman Zirvi <usmanzirvi@gmail.com>
2019-06-11 12:00:35 -0400

    Usman added comments about what he learned

--- Runner.java
+++ Runner.java
@@ -6,3 +6,7 @@ public class Runner{
         System.out.print(perfectSquare.perfectSquare(5));  
     }
 }
+
+//Usman: I've learned about how lambda functions are more effecient than standard functions, as writing the terms 
+//for the  ouput can be made to my liking in an easier and more direct approach. I've also learned how to clone repositories,
+ //create and merge branches, and push and pull commits.
